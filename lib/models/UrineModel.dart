dac52b4e44bfd3a40d60f6bbed1c3f0b8628f9b0
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2023-10-23 03:41:36 +0600

    changes in model

--- lib/models/UrineModel.dart
+++ lib/models/UrineModel.dart
@@ -1,13 +1,23 @@
-class Urine{
+class Urine {
   double volume;
   String color;
   final String time;
+  late final String date;
+
   Map<String, dynamic> toMap() {
     return {
       'volume': volume,
       'color': color,
+      'date': date,
       'time': time,
     };
   }
-  Urine({required this.volume, required this.color, required this.time});
+
+  void DateFormatter(DateTime date){
+    this.date = date.year.toString() + "-" + date.month.toString() + "-" + date.day.toString();
+  }
+
+  Urine({required this.volume, required this.color, required this.time}) {
+    DateFormatter(DateTime.now()); // Call DateFormatter in the constructor
+  }
 }
