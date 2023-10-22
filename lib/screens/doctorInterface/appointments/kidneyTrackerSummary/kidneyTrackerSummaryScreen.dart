aaf391afc42b30c5c56867ead1cc2e4ee2f0cbdf
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2023-10-23 03:42:57 +0600

    changes for adding all the screens in tracker summary page

--- lib/screens/doctorInterface/appointments/kidneyTrackerSummary/kidneyTrackerSummaryScreen.dart
+++ lib/screens/doctorInterface/appointments/kidneyTrackerSummary/kidneyTrackerSummaryScreen.dart
@@ -1,3 +1,5 @@
+import 'package:design_project_1/screens/doctorInterface/appointments/kidneyTrackerSummary/proteinSummaryScreen.dart';
+import 'package:design_project_1/screens/doctorInterface/appointments/kidneyTrackerSummary/urineSummaryScreen.dart';
 import 'package:design_project_1/screens/doctorInterface/appointments/kidneyTrackerSummary/waterSummaryScreen.dart';
 import 'package:design_project_1/screens/doctorInterface/appointments/kidneyTrackerSummary/weightSummaryScreen.dart';
 import 'package:flutter/material.dart';
@@ -20,8 +22,20 @@ class KidneyTrackerSummaryScreen extends StatefulWidget {
 }
 
 class _KidneyTrackerSummaryScreenState extends State<KidneyTrackerSummaryScreen> {
+  String selectedDuration = 'Past Week';
+  Map<String, int> durationMap = {
+    'Past Week': 7,
+    'Past Month': 30,
+    'Past Year': 365,
+  };
 
-
+  void updateDays(String? duration) {
+    if (duration != null) {
+      setState(() {
+        selectedDuration = duration;
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,15 +43,60 @@ class _KidneyTrackerSummaryScreenState extends State<KidneyTrackerSummaryScreen>
         title: Text('Kidney Tracker Summary'),
         backgroundColor: Colors.blue[900],
       ),
-      body: Center(
-        child:// Show loading indicator
-             SizedBox(
-          height: 400,
-          child: WeightSummary(
-            patientId: widget.patientId
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Choose Duration Format:',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(width: 20.0),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: DropdownButton<String>(
+                    value: selectedDuration,
+                    onChanged: (String? newValue) {
+                      updateDays(newValue);
+                    },
+                    items: <String>[
+                      'Past Week',
+                      'Past Month',
+                      'Past Year',
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          Container(
+            height: 600,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                UrineSummary(patientId: widget.patientId, days: durationMap[selectedDuration]),
+                WeightSummary(patientId: widget.patientId, days: durationMap[selectedDuration]),
+                WaterSummary(patientId: widget.patientId, days: durationMap[selectedDuration]),
+                BloodPressureSummary(patientId: widget.patientId, days: durationMap[selectedDuration]),
+                ProteinSummary(patientId: widget.patientId, days: durationMap[selectedDuration]),
+              ],
+            ),
+          ),
+        ],
       ),
     );
+
   }
 }
