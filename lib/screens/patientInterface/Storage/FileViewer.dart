ff61c217d0d5257786ebf0ef2995c16e6f88c02c
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2024-01-14 22:01:09 +0600

    patient side of ShareDocuments

--- lib/screens/patientInterface/Storage/FileViewer.dart
+++ lib/screens/patientInterface/Storage/FileViewer.dart
@@ -1,5 +1,8 @@
 import 'package:easy_pdf_viewer/easy_pdf_viewer.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class FileViewer extends StatefulWidget {
   final String URL;
@@ -16,17 +19,60 @@ class _FileViewerState extends State<FileViewer> {
   @override
   void initState() {
     super.initState();
-    initializePDF();
+    initialize();
   }
 
   PDFDocument? document;
 
-  void initializePDF() async{
-    document = await PDFDocument.fromURL(widget.URL);
-    setState(() {
+//   void initialize() async{
+//
+//     document = await PDFDocument.fromURL(widget.URL);
+//     if(document == null)
+//       {
+//         Fluttertoast.showToast(
+//           msg: 'This file can not be fetched at this moment',
+//           toastLength: Toast.LENGTH_SHORT,
+//           gravity: ToastGravity.BOTTOM,
+//           timeInSecForIosWeb: 1,
+//           backgroundColor: Colors.white,
+//           textColor: Colors.black,
+//         );
+//
+//       }
+// Navigator.pop(context);
+//     setState(() {
+//
+//     });
+//   }
 
-    });
+  void initialize() async {
+    try {
+      document = await PDFDocument.fromURL(widget.URL);
+      if (document == null) {
+        showFetchErrorToast();
+        Navigator.pop(context);
+      }
+    } catch (e) {
+      print("Error initializing file: $e");
+      showFetchErrorToast();
+      Navigator.pop(context);
+    }
+
+    setState(() {});
   }
+
+  void showFetchErrorToast() {
+    Fluttertoast.showToast(
+      msg: 'This file could not be fetched at this moment',
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 2,
+      backgroundColor: Colors.white,
+      textColor: Colors.black,
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     bool isImage = widget.URL.contains('.JPG') || widget.URL.endsWith('.png') || widget.URL.endsWith('.img');
--- lib/screens/patientInterface/viewAppointment/viewAppointmentDetails.dart
+++ lib/screens/patientInterface/viewAppointment/viewAppointmentDetails.dart
@@ -18,7 +18,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 import '../../../components/virtualConsultation/call.dart';
 import '../../../models/AppointmentModel.dart';
-import '../BookAppointment/ShareDocuments.dart';
+import 'ShareDocuments.dart';
 
 class ViewAppointmentDetailsPage extends StatefulWidget {
   final Appointment appointment;
@@ -221,7 +221,7 @@ class _ViewAppointmentDetailsPageState extends State<ViewAppointmentDetailsPage>
                 backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
               ),
               onPressed: () {
-                // Navigator.push(context, MaterialPageRoute(builder: (context) => ShareDocuments()));
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SharedDocuments(doctorID: widget.appointment.doctorId)));
               },
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -231,7 +231,7 @@ class _ViewAppointmentDetailsPageState extends State<ViewAppointmentDetailsPage>
                     color: Colors.black,
                   ),
                   SizedBox(width: 8.0), // Adjust the spacing between the icon and text
-                  Text('Share your Reports and Prescriptions')
+                  Text('Your shared Reports and Prescriptions')
                 ],
               ),
             ),
@@ -258,29 +258,11 @@ class _ViewAppointmentDetailsPageState extends State<ViewAppointmentDetailsPage>
                 ],
               ),
             ) :
-            // TextButton(
-            //   style: ButtonStyle(
-            //     backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-            //   ),
-            //
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.start,
-            //     children: [
-            //       Icon(
-            //         Icons.call, // Replace with the icon you want
-            //         color: Colors.black,
-            //       ),
-            //       SizedBox(width: 8.0), // Adjust the spacing between the icon and text
-            //       Icon(Icons.location_on),
-            //       Text('Chamber Address : ${doctorData?['chamberAddress']}'),
-            //
-            //     ],
-            //   ),
-            // ),
-                        ListTile(
-                          leading: Icon(Icons.location_on),
-                          title: Text('Chamber Address : ${doctorData?['chamberAddress']}'),
-                        ),
+
+            ListTile(
+               leading: Icon(Icons.location_on),
+                title: Text('Chamber Address : ${doctorData?['chamberAddress']}'),
+                ),
           ],
         ),
       ),
