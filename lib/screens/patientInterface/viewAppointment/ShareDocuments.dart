b6d6e4cdeb911629fd2d6fb1ee56dc724c451226
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2024-01-16 21:55:24 +0600

    Remove comments

--- lib/screens/patientInterface/viewAppointment/ShareDocuments.dart
+++ lib/screens/patientInterface/viewAppointment/ShareDocuments.dart
@@ -141,16 +141,7 @@ class _SharedDocumentsState extends State<SharedDocuments> {
                               _showDeleteConfirmationDialog(allFilesData[index]['id']);
                             },
                             child:
-                            // Card(
-                            //   color: Colors.white,
-                            //   child: ListTile(
-                            //     leading: Icon(Icons.file_copy),
-                            //     title: Text(
-                            //       allFilesData[index]['name'],
-                            //       style: TextStyle(color: Colors.black),
-                            //     ),
-                            //   ),
-                            // ),
+
                             Container(
                               margin: EdgeInsets.only(bottom: 15),
                               decoration: BoxDecoration(
